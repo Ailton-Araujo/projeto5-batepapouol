d54460fa41c7156202e2f3e51f658812068c1cff
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-04-13 00:48:25 -0300

    Busca e renderiza as msgs

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,14 @@ input.addEventListener("keypress", function (event) {
 }
 );
 
-let test;
-
 function menuSidebar() {
-const classSidebar = document.querySelector(".sidebar");
-    if(classSidebar.classList.contains("hidden")){
+    const classSidebar = document.querySelector(".sidebar");
+    if (classSidebar.classList.contains("hidden")) {
         classSidebar.classList.remove("hidden");
         setTimeout(() => {
             classSidebar.querySelector(".menuSide").classList.add("show");
         }, 100);
-    }else{
+    } else {
         classSidebar.querySelector(".menuSide").classList.remove("show");
         setTimeout(() => {
             classSidebar.classList.add("hidden");
@@ -35,18 +33,17 @@ function loginServer() {
         name: input.value
     };
 
-    const loginUser = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', user)
+    const promiseLogin = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', user)
     document.querySelector(".userInput").classList.add("hidden");
     document.querySelector(".loading").classList.remove("hidden");
 
-    loginUser.then((reply) => {
+    promiseLogin.then((reply) => {
         if (reply.status === 200) {
             document.querySelector(".loginPage").classList.add("hidden");
             document.querySelector(".msgPage").classList.remove("hidden");
-
         }
     });
-    loginUser.catch((reply) => {
+    promiseLogin.catch((reply) => {
         if (reply.response.status === 400) {
             document.querySelector(".loading").classList.add("hidden");
             document.querySelector(".userInput").classList.remove("hidden");
@@ -55,8 +52,28 @@ function loginServer() {
     });
     input.value = "";
 }
+function renderMsg(){
+    const promiseMsg = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promiseMsg.then((reply) => {
+        msgData = reply.data;
+        let mensages = document.querySelector("main>ul");
+        mensages.innerHTML = ""
+        for(let i = 0; i < msgData.length; i++){
+            mensages.innerHTML += `
+            <li data-test="message" class="msg ${msgData[i].type}">
+                <p class="timer">(${msgData[i].time})</p>
+                <p><strong>${msgData[i].from}</strong> definir em um switch/case <strong>${msgData[i].to}</strong>: ${msgData[i].text}</p>
+            </li>`   
+        }
+        mensages.scrollIntoView({block: "end"});
+    });
+}
+
+
 
 conectServer('');
 setInterval(() => {
-     axios.post('https://mock-api.driven.com.br/api/vm/uol/status', user);
+    axios.post('https://mock-api.driven.com.br/api/vm/uol/status', user);
 }, 5000);
+renderMsg()
+setInterval(renderMsg, 3000);
